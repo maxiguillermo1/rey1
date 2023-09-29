25044c097dd333132fbb1617a5a8f9a983e10f9a
maxi <maxi@maxis-MacBook-Air.local>
2023-09-29 13:52:50 -0700

    her

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-// src/App.jsx
-
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import { Link, Element } from 'react-scroll';
+import { Element } from 'react-scroll';
 import './App.css';
 import About from './components/About';
 import Currents from './components/Currents';
 import Contact from './components/Contact';
-import Nav from './components/Nav';
+import Nav from './components/Nav';  // Import Nav
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Reyna from './components/Reyna'
-import purpleStar from './images/purple-star.jpg';
-import heart from './images/heart-title.jpg';
-
+import Reyna from './components/Reyna';
 
 function App() {
   const [isVisible, setIsVisible] = useState(true);
 
-  
   useEffect(() => {
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.5, // visibility threshold
+      threshold: 0.5,
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
@@ -46,61 +39,40 @@ function App() {
       }
     };
   }, []);
-  
 
   return (
-      <div className='bg-gradient-to-r from-aquamarine via-soft-aqua to-aquamarine min-h-screen px-4 md:px-8 lg:px-16 min-h-screen'> {/* Added min-h-screen */}
-        <nav style={{ 
-           position: 'sticky',
-           top: 0,
-           zIndex: 1000,
-           fontFamily: 'Protomo, sans-serif', 
-           fontSize: '23px', 
-           color:'white',
-           textShadow:  `
-           2px 2px 2px rgba(255,33,156,0.7), 
-           -2px -2px 2px rgba(255,33,156,0.7), 
-           2px -2px 2px rgba(255,33,156,0.7), 
-           -2px 2px 2px rgba(255,33,156,0.7)
-         `,
-           }} 
-           className="flex justify-center items-center p-10">
-          <ul className="flex space-x-10">
-          <li><Link to="about" smooth={true}>ABOUT ME</Link></li>
-          <li><Link to="currents" smooth={true}>CURRENT FAVORITES</Link></li>
-          <li><Link to="skills" smooth={true}>SKILLS</Link></li>
-          <li><Link to="contact" smooth={true}>CONTACT</Link></li>
-        </ul>
-        </nav>
-  
-        <div className="flex flex-col items-center p-4">
-          <div className="center-element"> {/* Added wrapper div */}
-            <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-              <Reyna />
-            </Element>
-          </div>
-          <div className="center-element"> {/* Added wrapper div */}
-            <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-              <About />
-            </Element>
-          </div>
-          <div className="center-element"> {/* Added wrapper div */}
-            <Element name="currents" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-              <Currents />
-            </Element>
-          </div>
-          <div className="center-element"> {/* Added wrapper div */}
-            <Element name="skills" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-              <Skills />
-            </Element>
-          </div>
-          <div className="center-element"> {/* Added wrapper div */}
-            <Element name="contact" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-              <Contact />
-            </Element>
-          </div>
+    <div className='bg-gradient-to-r from-aquamarine via-soft-aqua to-aquamarine min-h-screen px-4 md:px-8 lg:px-16 min-h-screen'>
+      <Nav />  {/* Use Nav component */}
+      
+      <div className="flex flex-col items-center p-4">
+        <div className="center-element">
+          <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+            <Reyna />
+          </Element>
+        </div>
+        <div className="center-element">
+          <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+            <About />
+          </Element>
+        </div>
+        <div className="center-element">
+          <Element name="currents" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+            <Currents />
+          </Element>
+        </div>
+        <div className="center-element">
+          <Element name="skills" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+            <Skills />
+          </Element>
+        </div>
+        <div className="center-element">
+          <Element name="contact" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+            <Contact />
+          </Element>
         </div>
       </div>
-    );
+    </div>
+  );
 }
+
 export default App;
