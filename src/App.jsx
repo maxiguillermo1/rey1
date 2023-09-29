7b6104042a11da0f9d0f2f8452a1d4406e8674f0
maxi <maxi@maxis-MacBook-Air.local>
2023-09-29 15:54:17 -0700

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Link, Element } from 'react-scroll';
+import { Element } from 'react-scroll';
 import './App.css';
 import About from './components/About';
 import Currents from './components/Currents';
@@ -8,7 +8,7 @@ import Contact from './components/Contact';
 import Nav from './components/Nav';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Reyna from './components/Reyna'
+import Reyna from './components/Reyna';
 import purpleStar from './images/purple-star.jpg';
 import heart from './images/heart-title.jpg';
 
@@ -45,33 +45,15 @@ function App() {
 
   return (
     <div className='bg-gradient-to-r from-aquamarine via-soft-aqua to-aquamarine min-h-screen px-4 md:px-8 lg:px-16'>
-      <Nav /> {/* Include the Nav component */}
+      <Nav />
       
       <div className="flex flex-col items-center p-4">
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '200px', paddingBottom: '5px' }}>
-          
-        </div>
-
-        
-
-        <Element name="name" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <Reyna />
-        </Element>
-        <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <About />
-        </Element>
-        
-        <Element name="currents" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <Currents />
-        </Element>
-
-        <Element name="skills" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <Skills />
-        </Element>
-
-        <Element name="contact" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <Contact />
-        </Element>
+        {/* Remove unnecessary white spaces */}
+        <Reyna />
+        <About />
+        <Currents />
+        <Skills />
+        <Contact />
       </div>
     </div>
   );
