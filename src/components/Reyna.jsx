8c1c3f7aa80838c264048440e1ba388e5df20a24
maxi <maxi@maxis-MacBook-Air.local>
2023-09-29 02:43:34 -0700

    hello

--- src/components/Reyna.jsx
+++ src/components/Reyna.jsx
@@ -1,55 +1,80 @@
 import React from 'react';
 import heart from '../images/heart-title.jpg';
-
 const styles = {
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    paddingTop: '50px', // Adjust this value for top spacing
-    paddingBottom: '200px', // Adjust this value for bottom spacing
-  },
-  header: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: '10%',
-    height: 'auto',
-    objectFit: 'cover',
-  },
-  h1: {
-    fontFamily: 'Protomo, sans-serif',
-    fontSize: '35px',
-    color: 'white',
-    textShadow: '3px 3px 3px rgba(277,0,137,0.7), -3px -3px 3px rgba(277,0,137,0.7), 3px -3px 3px rgba(277,0,137,0.7), -3px 3px 3px rgba(277,0,137,0.7)',
-    paddingLeft: '15px', // Adjust this value for centering
-    paddingRight: '15px', // Adjust this value for centering
-    marginTop: '50px',
-    textAlign: 'center',
-  },
-  reversedImage: {
-    width: '10%',
-    height: 'auto',
-    objectFit: 'cover',
-    transform: 'scaleX(-1)',
-  },
-  text: {
-    fontFamily: 'dogica, sans-serif',
-    fontSize: '13px',
-    color: 'white',
-    marginTop: '10px',
-    textShadow: `
-      2px 2px 2px rgba(277,0,137,0.6), 
-      -2px -2px 2px rgba(277,0,137,0.6), 
-      2px -2px 2px rgba(277,0,137,0.6), 
-      -2px 2px 2px rgba(277,0,137,0.6)
-    `,
-    textAlign: 'center',
-  },
-};
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      padding: '10%',
+      textAlign: 'center',
+    },
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingBottom: '75px',
+    },
+    image: {
+      width: '10%',
+      height: 'auto',
+      objectFit: 'cover',
+    },
+    h1: {
+      fontFamily: 'Protomo, sans-serif',
+      fontSize: '35px',
+      color: 'white',
+      textShadow: '3px 3px 3px rgba(277,0,137,0.7), -3px -3px 3px rgba(277,0,137,0.7), 3px -3px 3px rgba(277,0,137,0.7), -3px 3px 3px rgba(277,0,137,0.7)',
+      paddingLeft: '15px',
+      paddingRight: '15px',
+      marginTop: '50px',
+    },
+    reversedImage: {
+      width: '10%',
+      height: 'auto',
+      objectFit: 'cover',
+      transform: 'scaleX(-1)',
+    },
+    text: {
+        fontFamily: 'dogica, sans-serif',
+        fontSize: '13px',
+        color: 'white',
+        marginTop: '-65px', // Reduced margin to make it closer
+        paddingBottom: '200px',
+        textShadow: `
+          2px 2px 2px rgba(277,0,137,0.6), 
+          -2px -2px 2px rgba(277,0,137,0.6), 
+          2px -2px 2px rgba(277,0,137,0.6), 
+          -2px 2px 2px rgba(277,0,137,0.6)
+        `,
+      },
+    };
+  
+  // Media query for screens smaller than 768px (typical mobile screens)
+  const mobileStyles = {
+    container: {
+      padding: '5%',
+    },
+    h1: {
+      fontSize: '24px',
+      marginTop: '30px',
+    },
+    text: {
+      fontSize: '12px',
+      marginTop: '5px',
+      paddingBottom: '100px', // Adjust as needed for mobile
+    },
+  };
+  
+  // Combine styles for mobile and desktop
+  const combinedStyles = {
+    ...styles,
+    '@media (max-width: 768px)': mobileStyles,
+  };
+  
+  export { styles, combinedStyles };
+  
 
 function Reyna() {
   return (
