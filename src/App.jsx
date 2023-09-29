fba1073bd2160408a163f0e5c139bab704def605
maxi <maxi@maxis-MacBook-Air.local>
2023-09-28 21:43:13 -0700

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
 import { Link, Element } from 'react-scroll';
 import './App.css';
 import About from './components/About';
@@ -8,9 +7,9 @@ import Contact from './components/Contact';
 import Nav from './components/Nav';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Title from './components/Title';  // <- Import the new Title component
 
 import purpleStar from './images/purple-star.jpg';
-import heart from './images/heart-title.jpg';
 
 function App() {
   const [isVisible, setIsVisible] = useState(true);
@@ -46,75 +45,14 @@ function App() {
   return (
     <div className='bg-gradient-to-r from-aquamarine via-soft-aqua to-aquamarine min-h-screen px-4 md:px-8 lg:px-16'>
       <Nav /> {/* Include the Nav component */}
-      
-      <div className="flex flex-col items-center p-4">
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '200px', paddingBottom: '5px' }}>
-          <img
-            src={heart}
-            alt="heart"
-            style={{
-              width: '10%',
-              height: 'auto',
-              objectFit: 'cover',
-            }}
-          />
-          <h1
-            style={{
-              fontFamily: 'Protomo, sans-serif',
-              fontSize: '30px',
-              color: 'white',
-              textShadow: '3px 3px 3px rgba(277,0,137,0.7), -3px -3px 3px rgba(277,0,137,0.7), 3px -3px 3px rgba(277,0,137,0.7), -3px 3px 3px rgba(277,0,137,0.7)',
-              paddingLeft: '15px', // Adjust this value for centering
-              paddingRight: '15px', // Adjust this value for centering
-              marginTop: '50px',
-            }}
-          >
-            REYNA AGUIRRE OKAMUNE
-          </h1>
-          <img
-            src={heart}
-            alt="heart"
-            style={{
-              width: '10%',
-              height: 'auto',
-              objectFit: 'cover',
-              transform: 'scaleX(-1)',
-            }}
-          />
-        </div>
-
-        <p style={{
-          fontFamily: 'dogica, sans-serif',
-          fontSize: '13px',
-          color: 'white',
-          marginTop: '10px',
-          paddingBottom: '200px',
-          textShadow: `
-          2px 2px 2px rgba(277,0,137,0.6), 
-          -2px -2px 2px rgba(277,0,137,0.6), 
-          2px -2px 2px rgba(277,0,137,0.6), 
-          -2px 2px 2px rgba(277,0,137,0.6)
-        `,
-        }}>
-          aspiring software engineer
-        </p>
-
-        <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <About />
-        </Element>
-        
-        <Element name="currents" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <Currents />
-        </Element>
-
-        <Element name="skills" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <Skills />
-        </Element>
-
-        <Element name="contact" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-          <Contact />
-        </Element>
-      </div>
+      <Title /> {/* Include the Title component */}
+      {/* Add the rest of your components and JSX code below */}
+      <About />
+      <Currents />
+      <Contact />
+      <Projects />
+      <Skills />
+      {/* You can continue with the rest of your code/components here... */}
     </div>
   );
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import heart from '../images/heart-title.jpg';
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        //background: 'linear-gradient(to right, #5edfff, #81ebff, #5edfff)'
+    },
+    header: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageHeart: {
+        width: '10%',
+        height: 'auto'
+    },
+    flippedImageHeart: {
+        width: '10%',
+        height: 'auto',
+        transform: 'scaleX(-1)'  // This flips the image horizontally
+    },
+    h1: {
+        fontFamily: 'Protomo, sans-serif',
+        fontSize: '35px',
+        color: 'white',
+        textShadow: '3px 3px 3px rgba(255,33,156,0.9), -3px -3px 3px rgba(255,33,156,0.9), 3px -3px 3px rgba(255,33,156,0.9), -3px 3px 3px rgba(255,33,156,0.9)',
+        textAlign: 'center',
+        paddingTop: '50px',
+        paddingLeft: '30px',
+        paddingRight: '30px'
+    },
+    subTitle: {
+        fontFamily: 'dogica, sans-serif',
+        fontSize: '15px',
+        color: 'white',
+        textShadow: '2px 2px 2px rgba(255,33,156,0.7), -2px -2px 2px rgba(255,33,156,0.7), 2px -2px 2px rgba(255,33,156,0.7), -2px 2px 2px rgba(255,33,156,0.7)',
+        paddingTop: '20px'
+    }
+};
+
+export default function Title() {
+    return (
+        <div style={styles.container}>
+            <div style={styles.header}>
+                <img src={heart} alt="heart" style={styles.imageHeart} />
+                <h1 style={styles.h1}>REYNA AGUIRRE OKAMUNE</h1>
+                <img src={heart} alt="flipped heart" style={styles.flippedImageHeart} />
+            </div>
+            <div style={styles.subTitle}>
+                aspiring software engineer
+            </div>
+        </div>
+    );
+}
