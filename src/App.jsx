10dd43acccd2d8cca813d75fa37569f6e3c332e6
maxi <maxi@maxis-MacBook-Air.local>
2023-09-29 13:47:30 -0700

    her

--- src/App.jsx
+++ src/App.jsx
@@ -47,55 +47,60 @@ function App() {
     };
   }, []);
   
-  return (
-    <div className='bg-gradient-to-r from-aquamarine via-soft-aqua to-aquamarine min-h-screen px-4 md:px-8 lg:px-16 min-h-screen'> {/* Added min-h-screen */}
-      <nav style={{ 
-         position: 'sticky',
-         top: 0,
-         zIndex: 1000,
-         fontFamily: 'Protomo, sans-serif', 
-         fontSize: '23px', 
-         color:'white',
-         textShadow:  `
-         2px 2px 2px rgba(255,33,156,0.7), 
-         -2px -2px 2px rgba(255,33,156,0.7), 
-         2px -2px 2px rgba(255,33,156,0.7), 
-         -2px 2px 2px rgba(255,33,156,0.7)
-       `,
-         }} 
-         className="flex justify-center items-center p-10">
-        {/* ... (existing nav code) */}
-      </nav>
 
-      <div className="flex flex-col items-center p-4">
-        <div className="center-element"> {/* Added wrapper div */}
-          <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-            <Reyna />
-          </Element>
-        </div>
-        <div className="center-element"> {/* Added wrapper div */}
-          <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-            <About />
-          </Element>
-        </div>
-        <div className="center-element"> {/* Added wrapper div */}
-          <Element name="currents" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-            <Currents />
-          </Element>
-        </div>
-        <div className="center-element"> {/* Added wrapper div */}
-          <Element name="skills" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-            <Skills />
-          </Element>
-        </div>
-        <div className="center-element"> {/* Added wrapper div */}
-          <Element name="contact" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
-            <Contact />
-          </Element>
+  return (
+      <div className='bg-gradient-to-r from-aquamarine via-soft-aqua to-aquamarine min-h-screen px-4 md:px-8 lg:px-16 min-h-screen'> {/* Added min-h-screen */}
+        <nav style={{ 
+           position: 'sticky',
+           top: 0,
+           zIndex: 1000,
+           fontFamily: 'Protomo, sans-serif', 
+           fontSize: '23px', 
+           color:'white',
+           textShadow:  `
+           2px 2px 2px rgba(255,33,156,0.7), 
+           -2px -2px 2px rgba(255,33,156,0.7), 
+           2px -2px 2px rgba(255,33,156,0.7), 
+           -2px 2px 2px rgba(255,33,156,0.7)
+         `,
+           }} 
+           className="flex justify-center items-center p-10">
+          <ul className="flex space-x-10">
+          <li><Link to="about" smooth={true}>ABOUT ME</Link></li>
+          <li><Link to="currents" smooth={true}>CURRENT FAVORITES</Link></li>
+          <li><Link to="skills" smooth={true}>SKILLS</Link></li>
+          <li><Link to="contact" smooth={true}>CONTACT</Link></li>
+        </ul>
+        </nav>
+  
+        <div className="flex flex-col items-center p-4">
+          <div className="center-element"> {/* Added wrapper div */}
+            <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+              <Reyna />
+            </Element>
+          </div>
+          <div className="center-element"> {/* Added wrapper div */}
+            <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+              <About />
+            </Element>
+          </div>
+          <div className="center-element"> {/* Added wrapper div */}
+            <Element name="currents" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+              <Currents />
+            </Element>
+          </div>
+          <div className="center-element"> {/* Added wrapper div */}
+            <Element name="skills" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+              <Skills />
+            </Element>
+          </div>
+          <div className="center-element"> {/* Added wrapper div */}
+            <Element name="contact" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
+              <Contact />
+            </Element>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
-
 export default App;
