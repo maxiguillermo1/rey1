67496564c1126b5ea9a248197c14987b8d80fa5f
maxi <maxi@maxis-MacBook-Air.local>
2023-09-29 02:47:08 -0700

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import { Link, Element } from 'react-scroll';
-import './App.css';
-import About from './components/About';
-import Currents from './components/Currents';
-import Contact from './components/Contact';
-import Nav from './components/Nav';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Reyna from './components/Reyna';
-
-import purpleStar from './images/purple-star.jpg';
-import heart from './images/heart-title.jpg';
-
+import React, { useState, useEffect } from 'react';
+import Nav from './Nav'; // Assuming you have a Nav component
+import Reyna from './Reyna'; // Import your components
+import About from './About';
+import Currents from './Currents';
+import Skills from './Skills';
+import Contact from './Contact';
+import { Element } from 'react-scroll'; // Assuming you're using react-scroll for scrolling
 
 function App() {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5, // visibility threshold
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setIsVisible(true);
-          observer.unobserve(entry.target);
-        }
-      });
-    }, options);
-
-    const target = document.querySelector('#aboutMe');
-    if (target) {
-      observer.observe(target);
-    }
-
-    return () => {
-      if (target) {
-        observer.unobserve(target);
-      }
-    };
-  }, []);
+    // You can add logic to control the visibility of sections here
+    // For example, when a certain event occurs or based on scroll position
+    // setIsVisible(true) when you want to show the sections.
+  }, []); // Add dependencies as needed
 
   return (
-    <div className='bg-gradient-to-r from-aquamarine via-soft-aqua to-aquamarine min-h-screen px-4 md:px-8 lg:px-16'>
-      <Nav /> {/* Include the Nav component */}
-      
+    <div>
+      <Nav />
       <div className="flex flex-col items-center p-4">
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '200px', paddingBottom: '5px' }}>
-          
-        </div>
-
-        
-
+        {/* Include your sections here */}
         <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
           <Reyna />
         </Element>
         <Element name="about" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
           <About />
         </Element>
-        
         <Element name="currents" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
           <Currents />
         </Element>
-
         <Element name="skills" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
           <Skills />
         </Element>
-
         <Element name="contact" style={{ opacity: isVisible ? 1 : 0, width: '100%' }}>
           <Contact />
         </Element>
