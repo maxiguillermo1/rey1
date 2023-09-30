250fb6a37dece9d52ea79a841049958d8456ba9a
maxi <maxi@maxis-MacBook-Air.local>
2023-09-30 00:02:12 -0700

    pleasegit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 import { Link } from 'react-scroll';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-
 
 const navStyles = {
   position: 'sticky',
